9507e1fbe8e585fdd32f8d37412010372c9de66b
Yoriaantje <1904507hanckmann@zuyd.nl>
2020-03-19 12:52:13 +0100

    Working Test Script

--- code/DataBase - Acces Testing.py
+++ code/DataBase - Acces Testing.py
@@ -4,19 +4,22 @@ import os.path
 
 def connect_to_DB():
     print("Creating Database connection...")
-    path = os.getcwd()
-    conn = pyodbc.connect(r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};"
-                          r"DBQ=" + path + "\MuziekDatabase.accdb;")
-    return conn
+    path = os.path.abspath("Muziek.accdb")
+    print(path)
+    conn_str = (r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};"
+                r"DBQ=C:\\Users\\yoria\\OneDrive - ZuydHogeschool\\HS Zuyd\\B1A09 - Databases\\Summatief\\Pi4 Deel2\\code pi4\\code\\Muziek.accdb")
+    connection = pyodbc.connect(conn_str)
+    return connection
 
 
 def print_data(conn):
     cursor = conn.cursor()
-    for table in cursor.tables(tableType='TABLE'):
-        print(table.table_name.rstrip())
+    for table in cursor.tables(tableType="TABLE"):
+        print(table.table_name)
 
-    table_name = str(input("Table Name? ")).upper()
-    cursor.execute("SELECT dbo_" + table_name)
+    table_name = str(input("Table Name? "))
+    to_print = "SELECT * from " + table_name
+    cursor.execute(to_print)
 
     for row in cursor.fetchall():
         print(row)
