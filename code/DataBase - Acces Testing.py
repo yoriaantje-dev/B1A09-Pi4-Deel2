44201954ce47e3985d897aa83b593b1d228515b0
Yoriaantje <1904507hanckmann@zuyd.nl>
2020-03-19 11:49:18 +0100

    restructuring
    Also made it so it just prints

--- code/DataBase - Acces Testing.py
+++ code/DataBase - Acces Testing.py
@@ -3,37 +3,26 @@ import os.path
 
 
 def connect_to_DB():
-    print('connecting to database...')
+    print('Creating Database connection...')
     path = os.getcwd()
     conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};'
                           r'DBQ=' + path + '\MuziekDatabase.accdb;')
     return conn
 
 
+def print_data(conn):
+    cursor = conn.cursor()
+    cursor.execute('select * dbo_' + table_name)
+
+    for row in cursor.fetchall():
+        print(row)
+
+
 def disconnect_sql(conn):
     conn.close()
 
 
-def show_destination(conn):
-    bestemmingen = conn.cursor()
-    SelectString = 'SELECT Plaats, Land, Werelddeel, Bestemmingcode FROM Bestemming;'
-
-    bestemmingen.execute(SelectString)
-    bestemmingenList = bestemmingen.fetchall()
-
-    print(len(bestemmingenList), 'bestemmingen : ')
-
-    for bestemming in bestemmingenList:
-        print('   ', bestemming.Plaats, bestemming.Land, bestemming.Werelddeel)
-    print('')
-
-
-# ---------------------------- MAIN PROGRAM -----------------------------------------
-database = connect_to_DB()
-show_destination(database)
-disconnect_sql(database)
-
-
-
-
-
+if name == __main__:
+    database = connect_to_DB()
+    show_destination(database)
+    disconnect_sql(database)
